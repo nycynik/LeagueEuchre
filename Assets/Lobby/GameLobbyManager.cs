b36d8fdbdeb023ab2721d4a6e9cba6d95f3e8a92
michael lynch <nycynik@gmail.com>
2017-01-05 00:37:10 -0500

    Added game lobby and allows users to choose opponents.  Should probably save the opponents in a game object, but that is saved for once we have a game object.

---
+++ Assets/Lobby/GameLobbyManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLobbyManager : MonoBehaviour {
+
+	private List<int> playerDifficulties = new List<int>() { 0, 1, 1, 1 };
+	private List<string> playerNames = new List<string>() { "Human", "AI\nEasy", "AI\nNormal", "AI\nBoss" };
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangePlayer (int playerId) {
+		if (playerId < playerDifficulties.Count) {
+			playerDifficulties [playerId] += 1;
+			if (playerDifficulties [playerId] >= playerNames.Count) {
+				playerDifficulties [playerId] = 0;
+			}
+				
+			// now update the text
+			GameObject curPlayerDiff = GameObject.Find ("Player" + (playerId+1));
+			Debug.Log ("Player" + (playerId+1));
+			curPlayerDiff.GetComponentInChildren<Text>().text = playerNames[playerDifficulties [playerId]];
+		}
+	}
+}
---
+++ Assets/Shared/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioClip[] soundClips;
+
+	private AudioSource audioSource;
+	private int nowPlaying = -1;
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	private void stopMusic () {
+		audioSource.Stop ();
+	}
+
+	private void playClip (int clipId) {
+
+		if (nowPlaying != clipId) {
+			AudioClip bgMusic = soundClips[clipId];
+			audioSource.clip = bgMusic;
+			audioSource.loop = true;
+			audioSource.Play ();
+			nowPlaying = clipId;
+		}
+	}
+
+	void OnLevelWasLoaded (int sceneId) {
+		Debug.Log (sceneId);
+		switch (sceneId) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			// play background music.
+			playClip(0);
+			break;
+		case 6:
+			stopMusic ();
+			break;
+		}
+
+
+	}
+}
